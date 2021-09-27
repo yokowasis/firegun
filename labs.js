864b03283abc8d3994580c858f204e7e5de3c1fb
yokowasis <yokowasis@gmail.com>
2021-09-27 21:50:09 +0800

    labs

--- labs.js
+++ labs.js
@@ -1,10 +1,28 @@
 const {Firegun} = require('./firegun');
 
-const fg = new Firegun(null,null,true);
+const fg = new Firegun(undefined,undefined,true);
 
 (async()=>{
-    fg.Get("level0/level1/level2/level3/0")
-    .then(data=>{
-        console.log (data.dummy);
+    fg.Put("test",{
+        a : {
+            "sub" : "sub-a",
+            "sub-sub" : {
+                "1level" : "deep",
+                "2level" : "deep",
+                "3level" : "deep",
+                "4level" : {
+                    "go" : "LOADDDDD",
+                    "5level" : {
+                        "yes" : "LOADDDDD"
+                    }
+                }
+            }
+        },
+        e : "Hello-e",
+        f : "Hello-f"
+    })
+    fg.Load("test")
+    .then(s=>{
+        console.log (s.a['sub-sub']);
     })
 })()
