f4c44db23b9544b058a66b28358f2902100b5a70
Wasis Haryo Sasoko <yokowasis@gmail.com>
2021-09-29 19:12:56 +0800

    Dev (#4)
    * masih stuck no hang ketika put pada userspace
    
    * test
    
    * send chat and retrieve wth optional encryption
    
    * test

--- labs.js
+++ labs.js
@@ -4,7 +4,18 @@ const fg = new Firegun([""],"fireDB",true);
 const chat = new Chat(fg);
 
 (async()=>{
-
+    await fg.userLogin("yokowasis2","123123123")
+    .then(async s=>{
+        console.log(s.pair);
+        if (s.err) {
+            console.log (s);
+        } else {
+            let pairkey = {
+                pub : "EX-Tsw_muNVHSW4txyehsls9j1RQnsb_pH09X2fKKys.AndN1oesf5svc7FkjVdOpkzJiiK_E8O0SZAPcE2EKrI",
+                epub : "ay9gY-BG2oBtwl-Dr_ElBb7Cb7qrRHAUCRpOWp6-hcs.2c4Nv8KA3M-k56fadsYZclEzppNm8dMa3X0jjEKGlcE"
+            }
+            let data = await chat.send(pairkey,"Hello with Encryption")
+            if (!data.err) {console.log ("OK")}
     await fg.userLogin("yokowasis2","123123123")
     .then(async s=>{
         if (s.err) {
--- yoko.js
+++ yoko.js
@@ -4,6 +4,15 @@ const fg = new Firegun([""],"fireDB",true,undefined,undefined,8766);
 const chat = new Chat(fg);
 
 fg.userLogin("yokowasis","123123123")
+.then(async s=>{
+    // console.log (fg.user.pair)
+    // chat.generatePublicCert()
+    let pairkey = {
+        pub : "Oz-Ik0Q2GSZvBXnveo53ly-2DGSqqBa8-qTvUGZnxmo.-b-aSPENXGTVDl0eumuNFbJj_T1mq4X3uoO77qGCt5A",
+        epub : "wsqzIoV8Bk7WbCUAnsfa6KEDIUlPqhqLXcmUZCSfMQw.U9Jf-P8_FWIgvqSRWhrPVrRIcFybHxTsDnCg3a5Ru1k"
+    }
+    let chats = await chat.retrieve(pairkey,["2021","9","29"]);
+    console.log (chats);
 .then(s=>{
     console.log (fg.user.pair)
     chat.generatePublicCert()
