d556605460348bf19ada3479ef64ec968a591da2
Wasis Sasoko - AIR <yokowasis@gmail.com>
2024-09-28 11:22:28 +0800

    server

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-import http from "http";
-import Gun from "gun";
+import express from 'express';
+import Gun from 'gun';
+import 'gun/axe.js'
 
-const server = http.createServer().listen(9482);
-const gun = Gun({ web: server });
+const port = 9482;
+
+var app = express();
+// @ts-ignore
+app.use(Gun.serve);
+app.use(express.static('./'));
+
+var server = app.listen(port);
+var gun = Gun({ file: 'data', web: server });
+
+console.log('Server started on port ' + port + ' with /gun');
