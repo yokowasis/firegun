f38432e3333aa756d76dae2790e78b08e40312cf
Wasis Sasoko - AIR <yokowasis@gmail.com>
2024-09-28 11:57:41 +0800

    index

---
+++ index.js
@@ -0,0 +1,2 @@
+export { default as Gun } from "gun";
+export { default as Firegun } from "./firegun";
--- index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Gun } from "gun";
-export { default as Firegun } from "./firegun";
-export { default as Chat }  from "./chat";
-export { common as common }  from "./common";
